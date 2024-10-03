b44a91d44f158c0b7f150cb5b4b707da1ce21e9d
F_K_CE_B <fatemeh78k78teh78@gmail.com>
2024-10-03 22:45:39 +0330

    part4-version1

--- app.py
+++ app.py
@@ -49,33 +49,83 @@ def Dashboard():
         return render_template('dashboard.html'  )
 
 
+################################################################################## fatemeh-k
 @app.route('/inputdata', methods=['GET' ,'POST'])
 def Inputdata():
     inputform = InputDataForm()
     
     if request.method =='POST':
+
         if inputform.validate_on_submit(): 
             imgfile = inputform.imgfile.data
             #save the uploaded file in upload_directory and save its info in database for further history
             feature1 = inputform.feature1.data
+            diagnosis = inputform.diagnosis.data
+            radius_mean = inputform.radius_mean.data
+            texture_mean = inputform.texture_mean.data
+            perimeter_mean = inputform.perimeter_mean.data
+            area_mean = inputform.area_mean.data
+            smoothness_mean = inputform.smoothness_mean.data
+            compactness_mean = inputform.compactness_mean.data
+            concavity_mean = inputform.concavity_mean.data
+            concave_points_mean = inputform.concave_points_mean.data
+            symmetry_mean = inputform.symmetry_mean.data
+            fractal_dimension_mean = inputform.fractal_dimension_mean.data
+            radius_se = inputform.radius_se.data
+            texture_se = inputform.texture_se.data
+            perimeter_se = inputform.perimeter_se.data
+            area_se = inputform.area_se.data
+            smoothness_se = inputform.smoothness_se.data
+            compactness_se = inputform.compactness_se.data
+            concavity_se = inputform.concavity_se.data
+            concave_points_se = inputform.concave_points_se.data
+            symmetry_se = inputform.symmetry_se.data
+            fractal_dimension_se = inputform.fractal_dimension_se.data
+            radius_worst = inputform.radius_worst.data
+            perimeter_worst = inputform.perimeter_worst.data
+            area_worst = inputform.area_worst.data
+            smoothness_worst = inputform.smoothness_worst.data
+            compactness_worst = inputform.compactness_worst.data
+            concavity_worst = inputform.concavity_worst.data
+            concave_points_worst = inputform.concave_points_worst.data
+            symmetry_worst = inputform.symmetry_worst.data
+            fractal_dimension_worst = inputform.fractal_dimension_worst.data
+            
             #save other features in database too (for further history)
-            input_object = {'imgfile': imgfile , 'feature1' :feature1}
+            input_object = {
+                            'imgfile': imgfile , 
+                            'feature1' :feature1 , 
+                            'diagnosis' :diagnosis , 'radius_mean' :radius_mean ,
+                            'texture_mean' :texture_mean , 'perimeter_mean' :perimeter_mean ,
+                            'area_mean' :area_mean , 'smoothness_mean' :smoothness_mean , 'compactness_mean' :compactness_mean , 
+                            'concavity_mean' :concavity_mean , 'concave_points_mean' :concave_points_mean , 'symmetry_mean' :symmetry_mean , 
+                            'fractal_dimension_mean' :fractal_dimension_mean , 'radius_se' :radius_se , 'texture_se' :texture_se , 'perimeter_se' :perimeter_se , 
+                            'area_se' :area_se , 'smoothness_se' :smoothness_se , 'compactness_se' :compactness_se , 'concavity_se' :concavity_se , 
+                            'concave_points_se' :concave_points_se , 'symmetry_se' :symmetry_se , 'fractal_dimension_se' :fractal_dimension_se , 'radius_worst' :radius_worst , 
+                            'perimeter_worst' :perimeter_worst , 'area_worst' :area_worst , 'smoothness_worst' :smoothness_worst , 'compactness_worst' :compactness_worst , 
+                            'concavity_worst' :concavity_worst , 'concave_points_worst' :concave_points_worst , 'symmetry_worst' :symmetry_worst , 'fractal_dimension_worstt' :fractal_dimension_worst
+
+                            }
+
             predict = PredictionModel(input_object)
             predicted_results =predict.computed_predictions()
             return render_template('prediction.html' , predicted_results=predicted_results )
     
     return render_template('inputdata.html', form=inputform  )
-
+##################################################################################
 
 @app.route('/history', methods=['GET'])
 def History():
         # extract History from database
         return render_template('history.html'  )
 
+################################################################################## fatemeh-k
+
 @app.route('/prediction', methods=['GET'])
 def Prediction(predicted_results):
     return render_template('prediction.html' , predicted_results )
 
+##################################################################################
 
 
 
